d074c731d4f60914bad653c8a8572caa1ce44f99
Yarden Zamir <dev@yarden-zamir.com>
2025-12-24 01:24:06 +0200

    fix: config reload on world join

--- src/main/kotlin/settingdust/item_converter/client/ClientEntrypoint.kt
+++ src/main/kotlin/settingdust/item_converter/client/ClientEntrypoint.kt
@@ -1,12 +1,18 @@
 package settingdust.item_converter.client
 
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent
 import settingdust.item_converter.ClientConfig
 import settingdust.item_converter.compat.ae2.AE2Compat
+import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 
 object ItemConverterClient {
     init {
         SlotInteractManager
-        ClientConfig.Companion.reload()
+        ClientConfig.reload()
         AE2Compat.init()
+
+        FORGE_BUS.addListener { _: ClientPlayerNetworkEvent.LoggingIn ->
+            ClientConfig.reload()
+        }
     }
 }
